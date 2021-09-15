38e43b264e6a52fad7a539b20876675dc59ace6b
Roshan Ranabhat <roshanranabhat11@gmail.com>
2021-09-15 08:39:26 +0545

    Study of JS data types

---
+++ index.js
@@ -0,0 +1,60 @@
+// Object
+// Array
+// Number
+// string
+// boolean
+// Float
+// DateTime
+// null
+// undefined
+
+let a = 1; // Number
+let b = "Hello World"; //string
+let c = true; //boolean
+let d = 1.5; //Float
+
+console.log(a);
+
+a = "IIMS";
+
+console.log(a);
+
+const example = "Hello World";
+console.log(example);
+
+
+let iims = null;
+console.log(iims);
+
+let person = {
+    name: 'Yajindra',
+    age: 21,
+    address: "Kathmandu",
+    phone: 9876543210,
+    location: {
+        lat: 27.7172453,
+        lng: 85.3239605
+    }
+};
+console.log("This person name is: ", person.name);
+console.log("His address is: ", person.address);
+// console.log("His Birth date is: ", person.birthDate);
+console.log("His location Latitude is: ", person.location.lat);
+console.log("His location Longitude is: ", person.location.lng);
+
+let exampleArray = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+let exampleArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+                //0,1,2,3,4,5,6,7,8,9,10
+
+console.log(exampleArray);
+console.log(exampleArray2);
+
+
+let studentNames = ["Yajindra", "Gaurav","Ram","Shyam"];
+console.log(studentNames[5])
+
+//variable
+//data types
+// object
+// array
+
