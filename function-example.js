98a70a01807ee4a197cc7dc4902baa70ee747bc0
Roshan Ranabhat <roshanranabhat11@gmail.com>
2021-09-17 08:39:08 +0545

    function examples

---
+++ function-example.js
@@ -0,0 +1,32 @@
+function exampleFunction(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+const example = () => "hello world";
+
+
+console.log(example());
+
+const getFullName = (firstName, lastName) => firstName.toUpperCase() + " " + lastName.toUpperCase();
+
+console.log(exampleFunction("John", "Doe"));
+console.log(getFullName("John", "Doe"));
+
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 24,
+  address: "Kathmandu",
+  getObject: function() {
+      return this;
+  },
+  getFullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+  getAge: function () {
+    return this.age;
+  },
+};
+// console.log("Person Full name is: ", person.getFullName());
+// console.log("Person Age is: ", person.getAge());
+console.log("Current object is: ", person.getObject());
--- index.js
+++ index.js
@@ -8,54 +8,54 @@
 // null
 // undefined
 
-// let a = 1; // Number
-// let b = "Hello World"; //string
-// let c = true; //boolean
-// let d = 1.5; //Float
+let a = 1; // Number
+let b = "Hello World"; //string
+let c = true; //boolean
+let d = 1.5; //Float
 
-// console.log(a);
+console.log(a);
 
-// a = "IIMS";
+a = "IIMS";
 
-// console.log(a);
+console.log(a);
 
-// const example = "Hello World";
-// console.log(example);
-
-
-// let iims = null;
-// console.log(iims);
-
-// let person = {
-//     name: 'Yajindra',
-//     age: 21,
-//     address: "Kathmandu",
-//     phone: 9876543210,
-//     location: {
-//         lat: 27.7172453,
-//         lng: 85.3239605
-//     },
-//     phone: ['1234567890','1231233434']
-// };
-// console.log("This person name is: ", person.name);
-// console.log("His address is: ", person.address);
-// // console.log("His Birth date is: ", person.birthDate);
-// console.log("His location Latitude is: ", person.location.lat);
-// console.log("His location Longitude is: ", person.location.lng);
-
-// let exampleArray = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-// let exampleArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//                 //0,1,2,3,4,5,6,7,8,9,10
-
-// console.log(exampleArray);
-// console.log(exampleArray2);
+const example = "Hello World";
+console.log(example);
 
 
-// let studentNames = ["Yajindra", "Gaurav","Ram","Shyam"];
-// console.log(studentNames[5])
+let iims = null;
+console.log(iims);
 
-//variable
-//data types
+let person = {
+    name: 'Yajindra',
+    age: 21,
+    address: "Kathmandu",
+    phone: 9876543210,
+    location: {
+        lat: 27.7172453,
+        lng: 85.3239605
+    },
+    phone: ['1234567890','1231233434']
+};
+console.log("This person name is: ", person.name);
+console.log("His address is: ", person.address);
+// console.log("His Birth date is: ", person.birthDate);
+console.log("His location Latitude is: ", person.location.lat);
+console.log("His location Longitude is: ", person.location.lng);
+
+let exampleArray = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+let exampleArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+                //0,1,2,3,4,5,6,7,8,9,10
+
+console.log(exampleArray);
+console.log(exampleArray2);
+
+
+let studentNames = ["Yajindra", "Gaurav","Ram","Shyam"];
+console.log(studentNames[5])
+
+// variable
+// data types
 // object
 // array
 
@@ -64,12 +64,11 @@
 let firstLetter = "Hello";
 let secondLetter = "World";
 let thirdLetter = firstLetter + secondLetter;
-// console.log("Length of third Letter is: ",thirdLetter.length);
+console.log("Length of third Letter is: ",thirdLetter.length);
 
-//slice start index, end index
-let example = "Hello world IIMS";
-// console.log(example.length);
-console.log(example.slice(-4));
+// slice start index, end index
+let exampleSlice = "Hello world IIMS";
+console.log(exampleSlice.slice(-5,-1));
 
 // concat two word
 let firstWord = "Hello ";
@@ -127,6 +126,8 @@ console.log(newWord.replaceAll("world","iims"));
 // pad a string example
 let padStartExample = "Hello";
 console.log(padStartExample.padStart(12,"IIMS"));
-
 let padEndExample = "World";
 console.log(padEndExample.padEnd(9,"IIMS"));
+
+
+
