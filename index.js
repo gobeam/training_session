43ad397b100a7f61a0caa51215b13f53ac914f6a
Roshan Ranabhat <roshanranabhat11@gmail.com>
2021-10-03 10:11:17 +0545

    Restful architecture explanation

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const http = require("http");
-const url = require("url");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((request, response) => {
-  let content = "";
-  let statusCode = 200;
-  switch (request.url) {
-    case "/":
-      content = "<h1>This is root page</h1>";
-      break;
-    case "/profile":
-      content = "<h1>This is profile page</h1>";
-      break;
-    case "/about":
-      content = "<h1>This is about page</h1>";
-      break;
-    default:
-      content = "<h1>This is 404 page</h1>";
-      statusCode = 404;
-  }
-
-  response.writeHead(statusCode, { "Content-Type": "text/html" });
-  response.write(content);
-  response.end();
+app.get("/",  (req, res) => {
+  res.json({
+    message: "Hello World"
+  });
 });
 
-const port = 3000;
-server.listen(port, () => {
-  console.log(`Server is running at port: ${port}`);
+app.get("/profile",  (req, res) => {
+  res.send("This is my profile");
 });
+
+app.listen(3000);
+
