ee8fa1681b6a1ec90f5f8a9191c11dbc1d1d9477
Roshan Ranabhat <roshanranabhat11@gmail.com>
2021-10-03 09:29:35 +0545

    node js server example

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const http = require("http");
 const url = require("url");
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(
-    "<h1>Hello IIMS</h1>"
-  );
+  let content = "";
+  let statusCode = 200;
+  switch (request.url) {
+    case "/":
+      content = "<h1>This is root page</h1>";
+      break;
+    case "/profile":
+      content = "<h1>This is profile page</h1>";
+      break;
+    case "/about":
+      content = "<h1>This is about page</h1>";
+      break;
+    default:
+      content = "<h1>This is 404 page</h1>";
+      statusCode = 404;
+  }
+
+  response.writeHead(statusCode, { "Content-Type": "text/html" });
+  response.write(content);
   response.end();
 });
 
