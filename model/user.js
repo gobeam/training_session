38804f2ca1c6dd35ff74397f804d4235db887dd8
roshanr <roshanranabhat11@gmail.com>
2021-10-21 08:36:54 +0545

    password hash implementation

--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
@@ -31,6 +32,14 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
