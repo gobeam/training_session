a94eee1bb896620e1d1933d42bf878c5cae9bae6
roshanr <roshanranabhat11@gmail.com>
2021-10-27 08:34:19 +0545

    login with jwt token starte

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUser, store, getById, update } = require("../controller/user-controller");
-const { getAll, addHobby } = require("../controller/hobby-controller");
+const {
+  getAllUser,
+  store,
+  getById,
+  update,
+  destroy,
+  login,
+} = require("../controller/user-controller");
 const UserValidator = require("../validator/user-validator");
+const LoginValidator = require("../validator/login-validator");
 const catchValidationError = require("../handler/validation-error-handler");
+
+router.post("/login", LoginValidator, catchValidationError(login));
 //get all users, Request Method: Get
 router.get("/users", getAllUser);
 
 // create new user, Request Method: Post
 router.post("/users", UserValidator, catchValidationError(store));
 
+// get user by id, Request Method: Get
 router.get("/users/:id", getById);
 
+// update user by id, Request Method: Put
 router.put("/users/:id", UserValidator, catchValidationError(update));
 
-// // get user by id, Request Method: Get
-// router.get("/users/:id", getById);
-
-// router.get("/hobby", getAll);
-// router.post("/hobby", addHobby);
+// delete user by id, Request Method: Delete
+router.delete("/users/:id", destroy);
 
 module.exports = router;
